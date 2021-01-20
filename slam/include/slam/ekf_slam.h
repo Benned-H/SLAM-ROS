5e433b0a8b54fe264920e3787ec1339af913c72f
Benned-H <benned1@gmail.com>
2021-01-19 23:40:49 -0600

    Backup EKF SLAM header file

--- slam/include/slam/ekf_slam.h
+++ slam/include/slam/ekf_slam.h
@@ -1,34 +1,38 @@
+/*
+ * Abstract class implementing EKF SLAM as defined on pg. 59 of Probabilistic Robotics, 3rd Edition
+ * Authors: Abrar Rahman and Benned Hedegaard
+ */
+
 #pragma once
 
 #include <iostream>
 #include <vector>
 #include <Eigen/Dense>
 
-#include "ros/ros.h"
+class EKFSLAM {
 
-#include "nav_msgs/Odometry.h"
-#include "geomery_msgs/Twist.h"
-
-class EKFSLAM{
     public:
+    
         EKFSLAM();
         virtual ~EKFSLAM();
-
-        void handle_input(const geometry_msgs::Twist::ConstPtr& msg);
-        void handle_odometry(const nav_msgs::Odometry::ConstPtr& msg);
-        void handle_observations(/* const perception::Observations::ConstPtr& msg */);
-        void step(const double& dt);
-
-        nav_msgs::Odometry estimated_odometry() const;
+        
+        // Add functions for updating _u and _z using whatever message types you'd like
+        
+        virtual Eigen::VectorXd g() const = 0; // Returns mu_bar = g( u, prev_mu )
+        virtual Eigen::VectorXd h() const = 0; // Returns z = h( x )
+        
+        virtual nav_msgs::Odometry estimated_odometry() const = 0; // Returns the current estimated odometry
+        
+        void step(const double& dt); // Steps the EKF belief forward
 
     private:
-        geometry_msgs::Twist _u;
-        perception::Observations _z;
-        std::vector<int> _observed_landmarks;
-        Eigen::VectorXd _mu;
-        Eigen::MatrixXd _sigma;
-        Eigen::VectorXd _alpha;
-        Eigen::MatrixXd _q;
+    
+        Eigen::VectorXd _mu; // Mean of Gaussian state representation
+        Eigen::MatrixXd _sigma; // Covariance of Gaussian state representation
+        Eigen::MatrixXd _G; // Jacobian of g
+        Eigen::MatrixXd _R; // 
+        Eigen::MatrixXd _H; // 
+        Eigen::MatrixXd _Q; // 
 };
 
 std::ostream& operator<<(std::ostream& out, const EKFSLAM& other);
